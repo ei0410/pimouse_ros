603ab83055b2282f987e262f895642d5a1ec22e0
Eiji Mori <chronolavos0124@gmail.com>
2018-05-12 09:27:13 +0000

    Modified .travis_ros_install_bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,12 +1,10 @@
 #!/bin/bash -xve
 
 #required packages
-#pip install --upgrade pip
-#hash -d pip
-#pip install catkin_pkg
-#pip install empy
-#pip install pyyaml
-#pip install rospkg
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
 
 #ros install
 cd ..
