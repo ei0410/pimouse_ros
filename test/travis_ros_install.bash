086bfdb954af305a41cc338ef934894437ee3b79
ei0410 <chronolavos0124@gmail.com>
2018-08-08 08:48:42 +0000

    travis

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,24 +1,21 @@
 #!/bin/bash -xve
 
-#required packages
-#sudo pip install --upgrade pip
+sudo pip install --upgrade pip
 #sudo apt-get install -y python3-venv
 #python3 -m venv env
 #source ./env/bin/activate
 #python -m pip install pip
-#pip install catkin_pkg
-#pip install empy
-#pip install pyyaml
-#pip install rospkg
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
 
-#ros install
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
-#catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
