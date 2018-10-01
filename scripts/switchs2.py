f86a27d86c8b38fd4c2a83b534f11c0e695f7910
ei0410 <chronolavos0124@gmail.com>
2018-10-01 06:08:05 +0000

    Add Switch code

---
+++ scripts/switchs2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys, rospy
+from pimouse_ros.msg import SwitchValues
+
+if __name__=='__main__':
+    devfile = '/dev/rtswitch1'
+#    devfile1 = '/dev/rtswitch1'
+#    devfile2 = '/dev/rtswitch2'
+    rospy.init_node('switchs')
+    pub = rospy.Publisher('switchs', SwitchValues, queue_size=1)
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        try:
+            with open(devfile,'r') as f:
+                data = f.readline().split()
+                data = [ int(e) for e in data ]
+                d = SwitchValues()
+                d.value = data[0]
+                pub.publish(d)
+                print d.value
+        except IOError:
+            rospy.logerr("cannot write to " + devfile)
+
+        rate.sleep()
